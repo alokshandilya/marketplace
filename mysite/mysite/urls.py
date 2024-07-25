acd138ef56b15d40a26085f1ef7ae1931a13b558
alokshandilya <alok.shandilya10@gmail.com>
2024-07-25 18:17:18 +0530

    add app `core`
    - formatter
    - test homepage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from core.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # homepage from core
+    path("admin/", admin.site.urls),
+    path("", index, name="index"),
 ]
