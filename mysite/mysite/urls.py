99dd7c23512988614395116982e621688bb451de
alokshandilya <alok.shandilya10@gmail.com>
2024-07-26 02:33:53 +0530

    contact page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from core.views import index
+from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
-    # homepage from core
-    path("admin/", admin.site.urls),
     path("", index, name="index"),
+    path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
