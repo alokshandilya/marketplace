8fbdf0cfbe9550aca1e6d563a7733d78c2db328e
alokshandilya <alok.shandilya10@gmail.com>
2024-07-27 02:33:17 +0530

    feat: Add Item model to item app
    - Adds the Item model to the item app.
    - Item model has fields for name, description, price, image, is_sold, created_by, created_at, and category.
    - Includes necessary meta options for ordering.
    - Registers the Item model in the admin site.

--- mysite/item/models.py
+++ mysite/item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -8,6 +9,25 @@ class Category(models.Model):
     class Meta:
         ordering = ("name",)
         verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    image = models.ImageField(upload_to="item_images/")
+    is_sold = models.BooleanField(default=False)
+    crated_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category, related_name="items", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ("name",)
 
     def __str__(self):
         return self.name
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from core.views import index, contact
@@ -24,4 +26,4 @@ urlpatterns = [
     path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
