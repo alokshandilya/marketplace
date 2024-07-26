60893f5bdb3b178ed9d4f053ebb30435030bc599
alokshandilya <alok.shandilya10@gmail.com>
2024-07-26 14:30:22 +0530

    Add Category model to item app
    - adds the Category model to the item app.
    - Category model has a name field and is used to categorize items.
    - includes the necessary meta options for ordering and plural name.

--- mysite/item/models.py
+++ mysite/item/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
-class Category (models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=255)
-    
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
