867c14738aa85a1b0e40b59e0c6012323fc50017
Alok Shandilya <alok.shandilya10@gmail.com>
2024-07-28 03:23:03 +0530

    Update URLs in base.html and index.html templates

--- mysite/item/views.py
+++ mysite/item/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Item
 
+
 # Create your views here.
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    return render(request, 'item/detail.html', {'item': item})
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(
+        pk=pk
+    )[0:3]
+    return render(
+        request, "item/detail.html", {"item": item, "related_items": related_items}
+    )
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,30 +1,11 @@
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("item/", include("item.urls")),
-    path("contact/", contact, name="contact"),
+    path("", include("core.urls")),
+    path("items/", include("item.urls")),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
